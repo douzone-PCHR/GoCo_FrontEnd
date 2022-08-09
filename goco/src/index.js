53652b7d242d6745e92d57bdf4b554eb7794b7d1
100SeongJun <94966290+100SeongJun@users.noreply.github.com>
2022-08-09 15:13:33 +0900

    Setting

--- goco/src/index.js
+++ goco/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './page/Login';
+import Main from './page/Main';
+import Search from './page/Search';
+import Signup from './page/Signup';
+import NotFound from './page/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/goco" element={<Main />} />
+      <Route path="" element={ }/>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
