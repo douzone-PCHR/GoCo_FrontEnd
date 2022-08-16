617622f5d7ebfd7685d1bf918c09ad37632aa9f6
100SeongJun <94966290+100SeongJun@users.noreply.github.com>
2022-08-16 09:49:06 +0900

    feat : 직원관리

--- goco/src/component/Admin/TableModalComponent.js
+++ goco/src/component/Admin/TableModalComponent.js
@@ -6,16 +6,33 @@ import teamPosition from './teamPosition.json';
 import { useEffect, useState } from 'react';
 import { getUnit } from '../../api/unitAPI';
 import { TableHeade } from './TableHead';
-
+import { updateEmp } from '../../api/employeeAPI';
+function updateDatafnc(updateData) {
+  const data = {
+    empNum: updateData.id,
+    jobTitle: {
+      jobTitleId: updateData.jobTitle,
+    },
+    teamPosition: {
+      teamPositionId: updateData.teamPosition,
+    },
+    unit: {
+      unitId: updateData.unit,
+    },
+  };
+  updateEmp(data);
+}
 export const TableModalComponent = ({ open, setOpen, result }) => {
   const [units, setUnit] = useState();
   const [selectDept, setSelectDept] = useState('');
   const [resultTeam, setResultTeam] = useState([]);
   const resultDept = [];
+  const [updateData, setUpdateData] = useState({ id: result.id });
+  console.log(result);
   useEffect(() => {
     getUnit(setUnit);
   }, []);
-
+  console.log(units);
   useEffect(() => {
     const teams = [];
     units &&
@@ -43,13 +60,25 @@ export const TableModalComponent = ({ open, setOpen, result }) => {
               {/* 이름 */}
               <TableCell align="center">{result.name}</TableCell>
               {/* 직급 */}
-              <Select data={jobTitle} title={'jobTitle'} />
+              <Select data={jobTitle} title={'jobTitle'} setUpdateData={setUpdateData} />
               {/* 부서 */}
-              <Select data={resultDept} title={'ParentUnit'} setUnit={setSelectDept} />
+              <Select
+                data={resultDept}
+                title={'ParentUnit'}
+                setUnit={setSelectDept}
+                setUpdateData={setUpdateData}
+              />
               {/* 팀 */}
-              {resultTeam && <Select data={resultTeam} title={'unit'} />}
+              {resultTeam && (
+                <Select data={resultTeam} title={'unit'} setUpdateData={setUpdateData} />
+              )}
               {/* 직책 */}
-              <Select data={teamPosition} title={'teamPosition'} result={result.teamPosition} />
+              <Select
+                data={teamPosition}
+                title={'teamPosition'}
+                result={result.teamPosition}
+                setUpdateData={setUpdateData}
+              />
               <TableCell align="center">
                 <div className={style.bgBlack}>{result.status === '1' ? '근무중 ' : '휴가'}</div>
               </TableCell>
@@ -58,7 +87,13 @@ export const TableModalComponent = ({ open, setOpen, result }) => {
         </Table>
         <Button
           onClick={() => {
+            // if (confirm('수정하시겠습니까?') == false) {
+            updateDatafnc(updateData);
             setOpen(false);
+            // } else {
+            // return false;
+            // }
+            // alert('수정하시겠습니까?');
           }}>
           수정
         </Button>
