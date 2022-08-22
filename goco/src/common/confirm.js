2a9a465b897a33128177639401c4d4916f5f943e
qqqwwwddd <qqqwwwddd1@gmail.com>
2022-08-22 11:33:45 +0900

    confirm target 추가

--- goco/src/common/confirm.js
+++ goco/src/common/confirm.js
@@ -1,9 +1,11 @@
 import Swal from 'sweetalert2';
+import '../common/confirm.css';
 
-export const deleteConfirm = (title, content) => {
+export const deleteConfirm = (title, text, target) => {
   return Swal.fire({
+    target: target,
     title: title,
-    text: '',
+    text: text,
     icon: 'warning',
 
     showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
@@ -16,10 +18,11 @@ export const deleteConfirm = (title, content) => {
   });
 };
 
-export const confirm = (title, content) => {
+export const confirm = (title, text, target) => {
   return Swal.fire({
+    target: target,
     title: title,
-    text: '',
+    text: text,
     icon: 'warning',
 
     showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
@@ -32,17 +35,35 @@ export const confirm = (title, content) => {
   });
 };
 
-export const addConfirm = (title, content) => {
+export const addConfirm = (title, text, target) => {
   return Swal.fire({
+    target: target,
     title: title,
-    text: '',
+    text: text,
     icon: 'warning',
 
     showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
     confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
     cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
-    confirmButtonText: '삭제', // confirm 버튼 텍스트 지정
+    confirmButtonText: '등록', // confirm 버튼 텍스트 지정
     cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+
+    reverseButtons: true, // 버튼 순서 거꾸로
+  });
+};
+
+export const resultConfirm = (title, text, icon, target) => {
+  return Swal.fire({
+    target: target,
+    title: title,
+    text: text,
+    icon: icon,
+
+    // showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+    confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
+    cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+    confirmButtonText: '확인', // confirm 버튼 텍스트 지정
+    // cancelButtonText: '취소', // cancel 버튼 텍스트 지정]
 
     reverseButtons: true, // 버튼 순서 거꾸로
   });
