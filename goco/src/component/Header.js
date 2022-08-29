a38f1feab5f52780e7179a0905b72927d691dfe9
JOMYEONGYOON <hairhold@naver.com>
2022-08-29 17:36:16 +0900

    header footer

--- goco/src/component/Header.js
+++ goco/src/component/Header.js
@@ -2,23 +2,19 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 import './header.css';
-import { FiMenu } from 'react-icons/fi';
-import { loginDefaultValue } from '../api/work/workAPI';
+
+import { headerData, loginDefaultValue } from '../api/work/workAPI';
+import HeaderComponent from './HeaderComponent';
 
 const Header = () => {
-  const [value, setValue] = useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   const [isNavVisible, setNavVisibility] = useState(false);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const [modeChange, setModeChange] = useState(0);
-  const [loginEmp, setLoginEmp] = useState();
-  const [urlValue, setUrlValue] = useState(window.location.href.split('http://localhost:3000/')[1]);
+  const [statusData, setStatusData] = useState();
+
   const mediaQuery = window.matchMedia('(max-width: 1600px)');
   useEffect(() => {
-    loginDefaultValue(setLoginEmp);
+    headerData(setStatusData);
 
     mediaQuery.addEventListener('change', handleMediaQueryChange);
     handleMediaQueryChange(mediaQuery);
@@ -27,7 +23,7 @@ const Header = () => {
       mediaQuery.removeEventListener('change', handleMediaQueryChange);
     };
   }, []);
-
+  console.log(statusData);
   const handleMediaQueryChange = (mediaQuery) => {
     if (mediaQuery.matches) {
       setIsSmallScreen(true);
@@ -36,64 +32,17 @@ const Header = () => {
     }
   };
 
-  const toggleNav = () => {
-    setNavVisibility(!isNavVisible);
-  };
-  console.log(loginEmp);
   return (
-    <>
-      {/* {modeChange === 0 && (
-        
-      )} */}
+    <header className="Header">
+      <img src={`${process.env.PUBLIC_URL}/assets/gocoLogo.png`} alt="logo" className="Logo" />
 
-      {/* 컴포넌트로 1개로 props로 전달  */}
-      {modeChange === 0 && (
-        <header className="Header">
-          <img src={`${process.env.PUBLIC_URL}/assets/gocoLogo.png`} alt="logo" className="Logo" />
-          <CSSTransition
-            in={!isSmallScreen || isNavVisible}
-            timeout={350}
-            classNames="NavAnimation"
-            unmountOnExit>
-            <nav className="Nav">
-              {/* 미리 배열로 선언해서 map 으로 뿌리는 방법 고려 */}
-              <Link
-                to="/goco"
-                onClick={() => setUrlValue('goco')}
-                style={{ color: urlValue === 'goco' ? '#00AAFF' : '#A8A8A8' }}>
-                팀원 근무 현황
-              </Link>
-              <Link
-                to="/approve"
-                onClick={() => setUrlValue('approve')}
-                style={{ color: urlValue === 'approve' ? '#00AAFF' : '#A8A8A8' }}>
-                요청 관리
-              </Link>
-              <Link
-                to="/board"
-                onClick={() => setUrlValue('board')}
-                style={{ color: urlValue === 'board' ? '#00AAFF' : '#A8A8A8' }}>
-                공지사항
-              </Link>
-
-              <Link to="/goco">
-                <button className="user-change-btn" onClick={() => setModeChange(0)}>
-                  사원 모드 전환
-                </button>
-              </Link>
-
-              <div className="nav-text">{loginEmp !== undefined && loginEmp.name}</div>
-              <div className="nav-text leader">개발 2팀 팀장</div>
-              {/* <button>Logout</button> */}
-            </nav>
-          </CSSTransition>
-
-          <button onClick={toggleNav} className="Burger">
-            <FiMenu />
-          </button>
-        </header>
+      {statusData !== undefined && (
+        <HeaderComponent
+          statusData={statusData}
+          modeChange={modeChange}
+          setModeChange={setModeChange}></HeaderComponent>
       )}
-    </>
+    </header>
   );
 };
 
