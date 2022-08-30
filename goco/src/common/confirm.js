01d53448d3cc5a820550cd39f8b9b0fa5cbf78cd
qqqwwwddd <qqqwwwddd1@gmail.com>
2022-08-30 15:04:26 +0900

    confirm 모달일때 아닐때 분리

--- goco/src/common/confirm.js
+++ goco/src/common/confirm.js
@@ -2,25 +2,40 @@ import Swal from 'sweetalert2';
 import '../common/confirm.css';
 
 export const deleteConfirm = (title, text, target) => {
-  return Swal.fire({
-    target: target,
-    title: title,
-    text: text,
-    icon: 'warning',
+  return target
+    ? Swal.fire({
+        target: target,
+        title: title,
+        text: text,
+        icon: 'warning',
 
-    showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
-    confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
-    cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
-    confirmButtonText: '삭제', // confirm 버튼 텍스트 지정
-    cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+        showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+        confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
+        cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+        confirmButtonText: '삭제', // confirm 버튼 텍스트 지정
+        cancelButtonText: '취소', // cancel 버튼 텍스트 지정
 
-    reverseButtons: true, // 버튼 순서 거꾸로
-  });
+        reverseButtons: true, // 버튼 순서 거꾸로
+      })
+    : Swal.fire({
+        title: title,
+        text: text,
+        icon: 'warning',
+
+        showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+        confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
+        cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+        confirmButtonText: '삭제', // confirm 버튼 텍스트 지정
+        cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+
+        reverseButtons: true, // 버튼 순서 거꾸로;
+      });
 };
 
 export const confirm = (title, text, target) => {
   return target
-    ? Swal.fire({
+    ? // modal 일 때
+      Swal.fire({
         target: target,
         title: title,
         text: text,
@@ -34,7 +49,8 @@ export const confirm = (title, text, target) => {
 
         reverseButtons: true, // 버튼 순서 거꾸로
       })
-    : Swal.fire({
+    : // modal 없을 때
+      Swal.fire({
         title: title,
         text: text,
         icon: 'warning',
@@ -50,35 +66,63 @@ export const confirm = (title, text, target) => {
 };
 
 export const addConfirm = (title, text, target) => {
-  return Swal.fire({
-    target: target,
-    title: title,
-    text: text,
-    icon: 'warning',
+  return target
+    ? Swal.fire({
+        target: target,
+        title: title,
+        text: text,
+        icon: 'warning',
 
-    showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
-    confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
-    cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
-    confirmButtonText: '등록', // confirm 버튼 텍스트 지정
-    cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+        showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+        confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
+        cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+        confirmButtonText: '등록', // confirm 버튼 텍스트 지정
+        cancelButtonText: '취소', // cancel 버튼 텍스트 지정
 
-    reverseButtons: true, // 버튼 순서 거꾸로
-  });
+        reverseButtons: true, // 버튼 순서 거꾸로
+      })
+    : Swal.fire({
+        title: title,
+        text: text,
+        icon: 'warning',
+
+        showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+        confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
+        cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+        confirmButtonText: '등록', // confirm 버튼 텍스트 지정
+        cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+
+        reverseButtons: true, // 버튼 순서 거꾸로
+      });
 };
 
 export const resultConfirm = (title, text, icon, target) => {
-  return Swal.fire({
-    target: target,
-    title: title,
-    text: text,
-    icon: icon,
+  return target
+    ? Swal.fire({
+        target: target,
+        title: title,
+        text: text,
+        icon: icon,
 
-    // showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
-    confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
-    cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
-    confirmButtonText: '확인', // confirm 버튼 텍스트 지정
-    // cancelButtonText: '취소', // cancel 버튼 텍스트 지정]
+        // showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+        confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
+        cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+        confirmButtonText: '확인', // confirm 버튼 텍스트 지정
+        // cancelButtonText: '취소', // cancel 버튼 텍스트 지정]
 
-    reverseButtons: true, // 버튼 순서 거꾸로
-  });
+        reverseButtons: true, // 버튼 순서 거꾸로
+      })
+    : Swal.fire({
+        title: title,
+        text: text,
+        icon: icon,
+
+        // showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+        confirmButtonColor: '#00aaff', // confrim 버튼 색깔 지정
+        cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+        confirmButtonText: '확인', // confirm 버튼 텍스트 지정
+        // cancelButtonText: '취소', // cancel 버튼 텍스트 지정]
+
+        reverseButtons: true, // 버튼 순서 거꾸로;
+      });
 };
